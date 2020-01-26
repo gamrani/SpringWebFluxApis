599dab5396f299d9897d7081f07f0e9f4495c246
gamraniyoussef <gamraniyoussef@gmail.com>
2020-01-26 14:08:05 +0100

    add card api

--- reactive-djo/src/main/java/com/reactive/djo/reactivedjo/controllers/CardController.java
+++ reactive-djo/src/main/java/com/reactive/djo/reactivedjo/controllers/CardController.java
@@ -2,22 +2,57 @@ package com.reactive.djo.reactivedjo.controllers;
 
 import com.reactive.djo.reactivedjo.models.Card;
 import com.reactive.djo.reactivedjo.services.CardService;
+import com.sun.org.apache.xalan.internal.xsltc.compiler.util.VoidType;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 @RestController
-@RequestMapping("/card")
+@RequestMapping("/cards")
 public class CardController {
 
     @Autowired
     private CardService cardService;
 
-    @GetMapping("/")
-    public Flux<Card> findAllCards(){
+    @GetMapping
+    public Flux<Card> getAllCards(){
         return cardService.getAllCards();
+    }
+
+    @GetMapping("{id}")
+    public Mono<ResponseEntity<Card>> getPoductById(@PathVariable String id){
+        return cardService.getCardById(id)
+                .map(ResponseEntity::ok)
+                .defaultIfEmpty(ResponseEntity.notFound().build());
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Mono<Card> saveCard(@RequestBody Card card){
+         return cardService.save(card);
+    }
+
+    @PutMapping("{id}")
+    public Mono<ResponseEntity<Card>> updateCard(@PathVariable(value="id") String id,
+                                                 @RequestBody Card card){
+        return cardService.updateCard(id, card)
+                .map(ResponseEntity::ok)
+                .defaultIfEmpty(ResponseEntity.notFound().build());
+    }
+
+    @DeleteMapping("{id}")
+    public Mono<ResponseEntity<Void>> deleteCard(@PathVariable String id){
+        return cardService.deleteCard(id)
+               .map(ResponseEntity::ok)
+                .defaultIfEmpty(ResponseEntity.notFound().build());
+    }
+
+    @DeleteMapping
+    public Mono<Void> deleteAllCards(){
+        return cardService.deleteAll();
     }
 
 }
--- reactive-djo/src/main/java/com/reactive/djo/reactivedjo/services/CardService.java
+++ reactive-djo/src/main/java/com/reactive/djo/reactivedjo/services/CardService.java
@@ -5,6 +5,7 @@ import com.reactive.djo.reactivedjo.repositories.CardRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 @Service
 public class CardService {
@@ -14,5 +15,31 @@ public class CardService {
 
     public Flux<Card> getAllCards(){
         return cardRepository.findAll();
+    }
+
+    public Mono<Card> getCardById(String id) {
+        return cardRepository.findById(id);
+    }
+
+    public Mono<Card> save(Card card) {
+        return cardRepository.save(card);
+    }
+
+    public Mono<Card> updateCard(String id, Card card) {
+        return cardRepository.findById(id)
+                .flatMap(existingCard -> {
+                    existingCard.setName(card.getName());
+                    existingCard.setType(card.getType());
+                    return cardRepository.save(existingCard);
+                });
+    }
+
+    public Mono<Void> deleteCard(String id) {
+        return cardRepository.findById(id)
+                .flatMap(cardRepository::delete);
+    }
+
+    public Mono<Void> deleteAll() {
+        return cardRepository.deleteAll();
     }
 }
