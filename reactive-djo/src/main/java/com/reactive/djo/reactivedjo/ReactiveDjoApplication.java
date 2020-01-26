b42c16bf350cb06d1f87e955fff68bafb922c352
gamraniyoussef <gamraniyoussef@gmail.com>
2020-01-26 11:49:11 +0100

    card data init

--- reactive-djo/src/main/java/com/reactive/djo/reactivedjo/ReactiveDjoApplication.java
+++ reactive-djo/src/main/java/com/reactive/djo/reactivedjo/ReactiveDjoApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ReactiveDjoApplication {
 
+
 	public static void main(String[] args) {
 		SpringApplication.run(ReactiveDjoApplication.class, args);
 	}
