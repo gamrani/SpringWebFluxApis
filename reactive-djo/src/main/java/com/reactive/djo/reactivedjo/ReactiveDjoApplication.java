ebdcf2b00da05c2aa8ff41f2f11c23f46715e37a
gamraniyoussef <gamraniyoussef@gmail.com>
2020-01-26 11:25:06 +0100

    project init

---
+++ reactive-djo/src/main/java/com/reactive/djo/reactivedjo/ReactiveDjoApplication.java
@@ -0,0 +1,13 @@
+package com.reactive.djo.reactivedjo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactiveDjoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactiveDjoApplication.class, args);
+	}
+
+}
---
+++ reactive-djo/src/test/java/com/reactive/djo/reactivedjo/ReactiveDjoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reactive.djo.reactivedjo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactiveDjoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
